431834ed835396e585199c14d5576f0f24b1cf62
Elie-Wakim <eliewakim28@gmail.com>
2026-06-17 12:00:20 +0300

    .

--- erp_app/invoice.py
+++ erp_app/invoice.py
@@ -1,5 +1,6 @@
+from apps.erpnext.erpnext.accounts.doctype.sales_invoice.sales_invoice import SalesInvoice
+
 import frappe
-from erpnext.accounts.doctype.sales_invoice.sales_invoice import SalesInvoice
 
 class CustomSalesInvoice(SalesInvoice):
     
@@ -7,12 +8,6 @@ class CustomSalesInvoice(SalesInvoice):
         super().validate()  
         frappe.msgprint(f"Invoice validated for {self.customer}", indicator="green")
     
-    def on_submit(self):
-        super().on_submit() 
-        frappe.log_error(
-            title="Sales Invoice Submitted",
-            message=f"Invoice {self.name} submitted for {self.customer} with total {self.grand_total}"
-        )
 def on_invoice_submit(doc, method):
     frappe.log_error(
         title="Sales Invoice Submitted",
